9fbb56bfd23a5619ce2740d3744aa6c289932962
Maksim Padrabinkin <averybodymax@gmail.com>
2024-07-18 20:15:31 +0200

    created GlobalStyle.js

---
+++ src/GlobalStyle.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    }
+
+    *, ::after, ::before {
+        box-sizing: inherit;
+    }
+
+    body {
+        background-color: ${({ theme }) => theme.color.whiteLilac};
+        /* font-family: "Open Sans", sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 400;
+        font-style: normal;
+        font-variation-settings: "wdth" 100; */
+    }
+`;
