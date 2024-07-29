15b4fb792c94176aa94eed106e2a2119ec4f0287
Maksim Padrabinkin <averybodymax@gmail.com>
2024-07-29 17:22:01 +0200

    styles replaced into another file

--- src/components/ProjectsSection/styled.js
+++ src/components/ProjectsSection/styled.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { ReactComponent as GithubIcon } from '../../images/mark-github.svg';
 
 export const Container = styled.article`
   padding: 0;
@@ -7,63 +6,6 @@ export const Container = styled.article`
   grid-template-columns: auto;
   grid-gap: 72px;
   align-items: center;
-`;
-
-export const HeaderWrapper = styled.header`
-  width: 195px;
-  margin: 72px auto 0;
-  text-align: center;
-
-  @media (max-width: 1200px) {
-    margin: 60px auto 0;
-  }
-  
-  @media (max-width: 480px) {
-    margin: 48px auto 0;
-  }
-`;
-
-export const GithubMark = styled(GithubIcon)`
-  color: ${({ theme }) => theme.color.scienceBlue};
-
-  @media (max-width: 480px) {
-    width: 32px;
-  }
-`;
-
-export const Header = styled.h2`
-  color: ${({ theme }) => theme.color.mineShaft};
-  font-size: 30px;
-  font-weight: 900;
-  line-height: 36px;
-  text-align: center;
-  margin: 12px 0 8px;
-
-  @media (max-width: 1200px) {
-    line-height: 29px;
-    font-size: 24px;
-  }
-
-  @media (max-width: 480px) {
-    line-height: 22px;
-    font-size: 18px;
-  }
-`;
-
-export const SubHeader = styled.p`
-  font-size: 20px;
-  font-weight: 400;
-  margin: 0;
-  color: ${({ theme }) => theme.color.mineShaft};
-
-  @media (max-width: 1200px) {
-    line-height: 26px;
-  }
-
-  @media (max-width: 480px) {
-    line-height: 24px;
-    font-size: 17px;
-  }
 `;
 
 export const Grid = styled.div`
