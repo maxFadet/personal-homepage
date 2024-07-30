7977fd0dee1bcd0e36e5862accfb82558d8e016b
Maksim Padrabinkin <averybodymax@gmail.com>
2024-07-30 15:13:53 +0200

    finalized Loading section

--- src/components/ProjectsSection/Loading/styled.js
+++ src/components/ProjectsSection/Loading/styled.js
@@ -1,16 +1,48 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { ReactComponent as Loader } from '../../../images/Loader.svg';
 
 export const Wrapper = styled.div`
-  max-width: 100%;
+   text-align: center;
+   margin: 88px 0 140px;
+
+   @media (max-width: 1200px) {
+    margin: 56px 0 94px;
+  }
+
+   @media (max-width: 480px) {
+    margin: 24px 0 48px;
+  }
 `;
 
 export const Text = styled.p`
   font-size: 20px;
   font-weight: 400;
   line-height: 28px;
+  margin: 0;
+`;
+
+const rotate = keyframes`
+  to {
+    transform: rotate(360deg);
+}
 `;
 
 export const LoadingIcon = styled(Loader)`
-  border: 11px solid rgba(3, 102, 214, 1)
+  margin-top: 48px;
+
+  circle:nth-child(2) {
+    stroke: ${({ theme }) => theme.color.scienceBlue};
+    animation: ${rotate} 2s linear infinite;
+    transform-origin: center;
+  }
+
+  @media (max-width: 1200px) {
+    width: 130px;
+    margin-top: 36px;
+  }
+
+  @media (max-width: 480px) {
+    width: 100px;
+    margin-top: 24px;
+  }
 `;
