81b844f76066b175ea9a257b5274c8fa0563c8d3
Maksim Padrabinkin <averybodymax@gmail.com>
2024-08-24 23:17:55 +0200

    added filtering of projects by description content

--- src/features/Homepage/ProjectsSection/projects.js
+++ src/features/Homepage/ProjectsSection/projects.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
-import { Loading } from "./Loading"
-import { Error } from "./Error"
+import { Loading } from "./Loading";
+import { Error } from "./Error";
 import {
     Grid,
     Tile,
@@ -28,22 +28,24 @@ const ProjectsSection = () => {
             {isError && <Error />}
             {!isLoading && !isError && (
                 <Grid>
-                    {projects.map(project => (
-                        <Tile key={project.id}>
-                            <Title>{project.name}</Title>
-                            <Description>{project.description}</Description>
-                            <Links>
-                                <Description>Demo: </Description>
-                                <Link href={project.homepage} target="_blank" rel="noopener noreferrer">
-                                    {project.homepage}
-                                </Link>
-                                <Description>Code: </Description>
-                                <Link href={project.html_url} target="_blank" rel="noopener noreferrer">
-                                    {project.html_url}
-                                </Link>
-                            </Links>
-                        </Tile>
-                    ))}
+                    {projects
+                        .filter(project => project.description)
+                        .map(project => (
+                            <Tile key={project.id}>
+                                <Title>{project.name}</Title>
+                                <Description>{project.description}</Description>
+                                <Links>
+                                    <Description>Demo: </Description>
+                                    <Link href={project.homepage} target="_blank" rel="noopener noreferrer">
+                                        {project.homepage}
+                                    </Link>
+                                    <Description>Code: </Description>
+                                    <Link href={project.html_url} target="_blank" rel="noopener noreferrer">
+                                        {project.html_url}
+                                    </Link>
+                                </Links>
+                            </Tile>
+                        ))}
                 </Grid>
             )}
         </>
