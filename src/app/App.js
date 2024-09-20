bc359dc9d66bdefa0d4bebf929df59730d678762
Maksim Padrabinkin <averybodymax@gmail.com>
2024-09-20 10:16:22 +0200

    Using normalize from the installed styled-normalize pack

--- src/app/App.js
+++ src/app/App.js
@@ -3,12 +3,14 @@ import GlobalStyle from "../theme/GlobalStyle";
 import Homepage from "../features/Homepage";
 import { lightTheme, darkTheme } from "../theme/theme";
 import { useSelector } from "react-redux";
+import { Normalize } from "styled-normalize"
 
 function App() {
     const darkMode = useSelector((state) => state.theme.darkMode);
 
     return (
         <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+            <Normalize />
             <GlobalStyle />
             <Homepage />
         </ThemeProvider>
