41ef4ef5803d8a7efff52b8807fd96bb3a6135be
Maksim Padrabinkin <averybodymax@gmail.com>
2024-10-20 12:39:47 +0200

    added a navigation arrow to the top of the page

--- src/features/Homepage/Footer/index.js
+++ src/features/Homepage/Footer/index.js
@@ -5,42 +5,55 @@ import {
   EmailLink,
   FooterText,
   SocialIcons,
-  SocialMediaLink
+  SocialMediaLink,
+  ScrollToTopButton,
 } from "./styled";
 import { resources } from "../../../components/Resources/resources";
 
-const Footer = () => (
-  <FooterWrapper>
-    <FooterContent>
-      <FooterHeading>
-        Let’s talk!
-      </FooterHeading>
-      <EmailLink
-        href={`mailto:${resources.email}`}
-        target="_blank"
-        title="My email"
-      >
-        {resources.email}
-      </EmailLink>
-      <FooterText>
-        I’m always open to new projects whenever I have the time.
-        If you have a website, dashboard or mobile app in mind and need
-        some help to make your ideas come to life, feel free to contact me 🤟
-      </FooterText>
-      <SocialIcons>
-        {resources.socialIcons.map((social, index) => (
-          <SocialMediaLink
-            key={index}
-            href={social.url}
-            target="_blank"
-            title={social.name}
-          >
-            <social.icon />
-          </SocialMediaLink>
-        ))}
-      </SocialIcons>
-    </FooterContent>
-  </FooterWrapper>
-);
+const Footer = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <FooterWrapper>
+      <FooterContent>
+        <FooterHeading>
+          Let’s talk!
+        </FooterHeading>
+        <EmailLink
+          href={`mailto:${resources.email}`}
+          target="_blank"
+          title="My email"
+        >
+          {resources.email}
+        </EmailLink>
+        <FooterText>
+          I’m always open to new projects whenever I have the time.
+          If you have a website, dashboard or mobile app in mind and need
+          some help to make your ideas come to life, feel free to contact me 🤟
+        </FooterText>
+        <SocialIcons>
+          {resources.socialIcons.map((social, index) => (
+            <SocialMediaLink
+              key={index}
+              href={social.url}
+              target="_blank"
+              title={social.name}
+            >
+              <social.icon />
+            </SocialMediaLink>
+          ))}
+        </SocialIcons>
+        <ScrollToTopButton onClick={scrollToTop}>
+          ↑
+        </ScrollToTopButton>
+      </FooterContent>
+    </FooterWrapper>
+  );
+};
 
 export default Footer;
